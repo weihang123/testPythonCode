c692bcc578c2f00b65e0db8c53ed0454bdb924ea
weihang123 <13582516932@163.com>
2018-08-28 10:57:45 +0800

    and_or代码提交

---
+++ venv/Include/test/and_or.py
@@ -0,0 +1,14 @@
+a =0
+
+b =0
+
+if(a and b):
+    print("1: 变量a 和 b 都为true")
+else:
+    print("1: 变量a 和 b 有一个不为true")
+
+if(a or b):
+    print("2: 变量a 和 b 有一个为true，那么就执行if语句表达式就true")
+
+else:
+    print("2: 变量a 和 b 都为false，那么就执行if语句表达式就false")
